c40c0ae4ad2b9af35d906f64339b96084cf50a2c
asaph <ace@dontletgo.com>
2019-11-11 21:22:43 -0800

    Add method to determine if the game is won

--- src/main/java/org/asaph/connect4/Board.java
+++ src/main/java/org/asaph/connect4/Board.java
@@ -8,22 +8,29 @@ import lombok.Getter;
 public class Board {
     public final static int DEFAULT_ROWS = 6;
     public final static int DEFAULT_COLUMNS = 7;
+    public final static int DEFAULT_CONNECT_THIS_MANY_TO_WIN = 4;
     @Getter
     private int rows;
     @Getter
     private int columns;
+    @Getter
+    private int connectThisManyToWin;
     private DiscColor[][] moves;
     private int[] rowHeights;
 
-    public Board(int rows, int columns) {
+    public Board(int rows, int columns, int connectThisManyToWin) {
+        if (connectThisManyToWin > Math.max(rows, columns)) {
+            throw new IllegalArgumentException("connectThisManyToWin value is too big for this board");
+        }
         this.rows = rows;
         this.columns = columns;
+        this.connectThisManyToWin = connectThisManyToWin;
         this.moves = new DiscColor[this.rows][this.columns];
         this.rowHeights = new int[this.columns];
     }
 
     public Board() {
-        this(DEFAULT_ROWS, DEFAULT_COLUMNS);
+        this(DEFAULT_ROWS, DEFAULT_COLUMNS, DEFAULT_CONNECT_THIS_MANY_TO_WIN);
     }
 
     public void move(int column, DiscColor discColor) {
@@ -34,6 +41,75 @@ public class Board {
             throw new IllegalArgumentException("Column " + column + " is full");
         }
         moves[rowHeights[column]++][column] = discColor;
+    }
+
+    public DiscColor getWinner() {
+        for (int r=0; r<rows; r++) {
+            for (int c=0; c<columns; c++) {
+                DiscColor discColor = this.moves[r][c];
+                if (discColor == null) {
+                    continue;
+                }
+                if (c+connectThisManyToWin<=columns) {
+                    // check horizontal win
+                    boolean win = true;
+                    for (int i=c+1; i<c+connectThisManyToWin; i++) {
+                        DiscColor nextDiscColor = this.moves[r][i];
+                        if (!discColor.equals(nextDiscColor)) {
+                            win = false;
+                            break;
+                        }
+                    }
+                    if (win) {
+                        return discColor;
+                    }
+                    if (r+connectThisManyToWin<=rows) {
+                        // check diagonal going up and right
+                        win = true;
+                        // check diagonal going up and left
+                        for (int i=c+1, j=r+1; j<r+connectThisManyToWin; i++, j++) {
+                            DiscColor nextDiscColor = this.moves[j][i];
+                            if (!discColor.equals(nextDiscColor)) {
+                                win = false;
+                                break;
+                            }
+                        }
+                        if (win) {
+                            return discColor;
+                        }
+                    }
+                }
+                if (r+connectThisManyToWin<=rows) {
+                    // check vertical win
+                    boolean win = true;
+                    for (int j=r+1; j<r+connectThisManyToWin; j++) {
+                        DiscColor nextDiscColor = this.moves[j][c];
+                        if (!discColor.equals(nextDiscColor)) {
+                            win = false;
+                            break;
+                        }
+                    }
+                    if (win) {
+                        return discColor;
+                    }
+                    if (connectThisManyToWin-1<=c) {
+                        win = true;
+                        // check diagonal going up and left
+                        for (int i=c-1, j=r+1; j<r+connectThisManyToWin; i--, j++) {
+                            DiscColor nextDiscColor = this.moves[j][i];
+                            if (!discColor.equals(nextDiscColor)) {
+                                win = false;
+                                break;
+                            }
+                        }
+                        if (win) {
+                            return discColor;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     @Override
--- src/test/java/org/asaph/connect4/BoardTest.java
+++ src/test/java/org/asaph/connect4/BoardTest.java
@@ -11,6 +11,7 @@ class BoardTest {
         Board board = new Board();
         assertEquals(Board.DEFAULT_ROWS, board.getRows());
         assertEquals(Board.DEFAULT_COLUMNS, board.getColumns());
+        assertEquals(Board.DEFAULT_CONNECT_THIS_MANY_TO_WIN, board.getConnectThisManyToWin());
 
         board.move(3, DiscColor.RED);
         board.move(3, DiscColor.BLACK);
@@ -31,5 +32,76 @@ class BoardTest {
     void testInvalidMoveIndexOutOfBounds() {
         Board board = new Board();
         assertThrows(IllegalArgumentException.class, () -> board.move(Board.DEFAULT_COLUMNS, DiscColor.RED));
+    }
+
+    @Test
+    void testGetWinnerEmptyBoard() {
+        Board board = new Board();
+        assertNull(board.getWinner());
+    }
+
+    @Test
+    void testRedWinsVertically() {
+        Board board = new Board();
+        board.move(3, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        board.move(3, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        board.move(3, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        assertNull(board.getWinner());
+        board.move(3, DiscColor.RED);
+        assertEquals(DiscColor.RED, board.getWinner());
+    }
+
+    @Test
+    void testRedWinsHorizontally() {
+        Board board = new Board();
+        board.move(3, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        board.move(4, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        board.move(5, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        assertNull(board.getWinner());
+        board.move(6, DiscColor.RED);
+        assertEquals(DiscColor.RED, board.getWinner());
+    }
+
+
+    @Test
+    void testRedWinsDiagonallyUpAndRight() {
+        Board board = new Board();
+        board.move(3, DiscColor.RED);
+        board.move(4, DiscColor.BLACK);
+        board.move(4, DiscColor.RED);
+        board.move(5, DiscColor.BLACK);
+        board.move(5, DiscColor.RED);
+        board.move(6, DiscColor.BLACK);
+        board.move(5, DiscColor.RED);
+        board.move(6, DiscColor.BLACK);
+        board.move(6, DiscColor.RED);
+        board.move(3, DiscColor.BLACK);
+        assertNull(board.getWinner());
+        board.move(6, DiscColor.RED);
+        assertEquals(DiscColor.RED, board.getWinner());
+    }
+
+    @Test
+    void testRedWinsDiagonallyUpAndLeft() {
+        Board board = new Board();
+        board.move(3, DiscColor.RED);
+        board.move(2, DiscColor.BLACK);
+        board.move(2, DiscColor.RED);
+        board.move(1, DiscColor.BLACK);
+        board.move(1, DiscColor.RED);
+        board.move(0, DiscColor.BLACK);
+        board.move(1, DiscColor.RED);
+        board.move(0, DiscColor.BLACK);
+        board.move(0, DiscColor.RED);
+        board.move(3, DiscColor.BLACK);
+        assertNull(board.getWinner());
+        board.move(0, DiscColor.RED);
+        assertEquals(DiscColor.RED, board.getWinner());
     }
 }
